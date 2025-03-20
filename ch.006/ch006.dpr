5d8d6c931bcaf11490ab8baa9fa58fd29dad70a0
DELMadang <delmadang95@gmail.com>
2025-03-20 15:13:57 +0900

    금액, 이자율, 기간으로 단순 이자율 구하기

---
+++ ch.006/ch006.dpr
@@ -0,0 +1,31 @@
+program ch006;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils;
+
+begin
+  try
+    WriteLn('원금 금액: ');
+    var principal: Double;
+    ReadLn(principal);
+
+    WriteLn('이자율(%): ');
+    var rate: Double;
+    ReadLn(rate);
+
+    WriteLn('기간(년): ');
+    var time: Double;
+    ReadLn(time);
+
+    var interest := (principal * rate * time) / 100;
+    WriteLn('단순 이자 총액: ', FormatFloat('#,##0', interest));
+    ReadLn;
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+end.
