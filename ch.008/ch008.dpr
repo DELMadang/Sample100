ef63001f4249f92874e445f6dfd255983e6af335
DELMadang <delmadang95@gmail.com>
2025-03-21 21:35:04 +0900

    자연수의 합계

---
+++ ch.008/ch008.dpr
@@ -0,0 +1,27 @@
+program ch008;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils;
+
+begin
+  try
+    WriteLn('숫자를 입력하세요: ');
+    var Number: Integer;
+    ReadLn(Number);
+
+    var sum := 0;
+    for var i := 1 to Number do
+      sum := sum + i;
+
+    WriteLn('자연수의 합: ', sum);
+
+    ReadLn;
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+end.
