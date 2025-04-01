0ac06a0a01d5510a091c7d1e44fd51fb7aa005ac
DELMadang <delmadang95@gmail.com>
2025-04-01 22:27:34 +0900

    Print the Pattern of a Diamond

---
+++ ch.043/ch043.dpr
@@ -0,0 +1,43 @@
+program ch043;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils;
+
+begin
+  try
+    Write('숫자를 입력하세요: ');
+    var rows: Integer;
+    ReadLn(rows);
+
+    for var i := 1 to rows do
+    begin
+      for var j := 1 to rows - i do
+        Write(' ');
+
+      for var k := 1 to 2 * i - 1 do
+        Write('*');
+
+      WriteLn('');
+    end;
+
+    for var i := rows - 1 downto 1 do
+    begin
+      for var j := 1 to rows - i do
+        Write(' ');
+
+      for var k := 1 to 2 * i - 1 do
+        Write('*');
+
+     WriteLn('');
+    end;
+
+    ReadLn;
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+end.
