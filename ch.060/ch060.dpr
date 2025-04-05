8df6ef7c327309db9b26c85e65f420e85cc6ec50
DELMadang <delmadang95@gmail.com>
2025-04-05 10:05:44 +0900

    Implement Bubble Sort

---
+++ ch.060/ch060.dpr
@@ -0,0 +1,36 @@
+program ch060;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils;
+
+begin
+  try
+    var arr := [5, 3, 4, 8, 2];
+    var n := High(arr);
+    for var i := 0 to n do
+    begin
+      for var j := 0 to n do
+      begin
+        if (arr[j] > arr[j + 1]) then
+        begin
+          var temp := arr[j];
+          arr[j] := arr[j + 1];
+          arr[j + 1] := temp;
+        end;
+      end;
+    end;
+
+    Write('Sorted array: ');
+    for var element in arr do
+      Write(element, ', ');
+
+    ReadLn;
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+end.
